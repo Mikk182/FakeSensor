d80a9253d6798bfdd81a2068c18abeb1e7b7d5e8
Mikk182 <mickael.chemin@gmail.com>
2016-10-26 00:53:51 +0200

    While(true) ;)

--- FakeSensor/Form1.cs
+++ FakeSensor/Form1.cs
@@ -27,15 +27,53 @@ namespace FakeSensor
             //      "Hello from the PubNub C# SDK!",
             //      null,null
             //);
+            Sensor[] sensorArray =
+            {
+                new Sensor{
+                    id = 11,
+                    lt = 49.4876274,
+                    lg = 0.5741083,
+                    gasrate = 8
+                  }
+            };
+
             pubnub.Publish<string>(
-                  "Sensors",
-                  new
-                  {id = 11, lt = 49.4876274, lg = 0.5741083, gasrate = 8 },
-                  true,
-                  "test",
-                  DisplayReturnMessage,
-                  DisplayErrorMessage
-            );
+                       "Sensors",
+                       //new {id = 11, latlng = new double[]{ 49.4876274, 0.5741083}, gasrate = 8 },
+                       sensorArray,
+                       true,
+                       "test",
+                       DisplayReturnMessage,
+                       DisplayErrorMessage
+                 );
+
+            while (true)
+            {
+                Random r = new Random();
+                int iNum;
+                int result;
+
+                iNum = r.Next(-10, 11); //put whatever range you want in here from negative to positive 
+                if (iNum == 0) continue;
+                result = iNum / (int)Math.Abs(iNum);
+
+                foreach(var sensor in sensorArray)
+                {
+                    sensor.lt = sensor.lt + (result * 0.0002);
+                    sensor.lg = sensor.lg + (result * 0.0002);
+                }
+                pubnub.Publish<string>(
+                       "Sensors",
+                       //new {id = 11, latlng = new double[]{ 49.4876274, 0.5741083}, gasrate = 8 },
+                       sensorArray,
+                       true,
+                       "test",
+                       DisplayReturnMessage,
+                       DisplayErrorMessage
+                 );
+                System.Threading.Thread.Sleep(3000);
+            }
+
         }
         void DisplayReturnMessage(string result)
         {
---
+++ FakeSensor/Sensor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakeSensor
+{
+    class Sensor
+    {
+        public int id { get; set; }
+        public double lt { get; set; }
+        public double lg { get; set; }
+        public int gasrate { get; set; }
+    }
+}
